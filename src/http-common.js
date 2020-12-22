83261e55dcb2a119273e8086750686c02ea9f5dd
sumesh3060 <sumesh3060@gmail.com>
2020-12-22 18:06:22 +0530

    be

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://flow-arjun.herokuapp.com/flows",
   headers: {
     "Content-type": "application/json"
   }
